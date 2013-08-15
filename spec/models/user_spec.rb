3b417d0a136c88ca96894f5f319e17ab49b93e93
Glob Villi <likeplaid@gmail.com>
2013-08-14 22:46:55 -0700

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,11 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when email has consecutive dots" do
+    before { @user.email = "foo@bar..com" }
+    it { should_not be_valid }
+  end
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -41,6 +46,16 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 
     describe "when password is not present" do
@@ -76,7 +91,7 @@ describe User do
   	end
 
 	describe "return value of authenticate method" do
-  	before { @user.save }
+  	 before { @user.save }
   	let(:found_user) { User.find_by(email: @user.email) }
 
   	describe "with valid password" do
